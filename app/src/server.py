a9e1d1b15da156dde1b8f3a6aaaa0d83229ddf84
Scott Wedge <scott.wedge@gmail.com>
2020-11-10 11:54:16 -0500

    Add two more web site links that return current time

--- app/src/server.py
+++ app/src/server.py
@@ -6,5 +6,13 @@ server = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@server.route("time1")
+def time1():
+    return "Current time is: datetime.datetime.now()"
+
+@server.route("time2")
+def time2():
+    return "Current time is: datetime.datetime.utcnow()"
+
 if __name__ == "__main__":
     server.run(host="0.0.0.0")
