52933e2d48fec29f1cdd5998355a86508e8a5fc0
Scott Wedge <scott.wedge@gmail.com>
2020-11-10 20:23:29 -0500

    Time is displayed but both show UTC for unknown reason

--- app/src/server.py
+++ app/src/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import datetime
 
 server = Flask(__name__)
 
@@ -8,13 +9,13 @@ def hello():
 
 @server.route("/time1")
 def time1():
-    ct = datetime.datetime.now()
-    return ct
+    nt = datetime.datetime.now()
+    return str(nt)
 
 @server.route("/time2")
 def time2():
-    ct = datetime.datetime.utcnow()
-    return ct
+    ut = datetime.datetime.utcnow()
+    return str(ut)
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=5000)
