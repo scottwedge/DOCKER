163d2d549d3315dd0a02fe987566c33214d28d3c
Scott Wedge <scott.wedge@gmail.com>
2020-11-10 19:17:04 -0500

    Was printing 'datetime....' instead of time, see if this fixes it

--- app/src/server.py
+++ app/src/server.py
@@ -8,11 +8,13 @@ def hello():
 
 @server.route("/time1")
 def time1():
-    return "Current time is: datetime.datetime.now()"
+    ct = datetime.datetime.now()
+    return ct
 
 @server.route("/time2")
 def time2():
-    return "Current time is: datetime.datetime.utcnow()"
+    ct = datetime.datetime.utcnow()
+    return ct
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0")
+    server.run(host="0.0.0.0", port=5000)
